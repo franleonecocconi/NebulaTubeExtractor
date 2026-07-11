6f613313260307089ac11dda2ba6fbf71fd1411f
Francesco Leone Cocconi <escuelitadetecnologiatechyt367@gmail.com>
2026-07-11 01:08:02 -0300

    GitHub Actions Script

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const http = require('http');
-const url = require('url');
-const { Innertube, UniversalCache } = require('youtubei.js');
+import http from 'http';
+import url from 'url';
+import { Innertube, UniversalCache } from 'youtubei.js';
 
 const PORT = process.env.PORT || 3000;
 
